4551b925fed6f821ad9abed64d4cb65e996688b5
Aven9 <961273881@qq.com>
2018-02-22 22:54:18 +0800

    second commit

---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings():
+    """save all the settings of alien invasion"""
+
+    def __init__(self):
+        """initialize the settings of game."""
+
+        # screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (100,100,100)
+
---
+++ ship.py
@@ -0,0 +1,27 @@
+import pygame
+
+
+class Ship:
+
+    def __init__(self, screen):
+        """初始化飞船并设置其位置"""
+        self.screen = screen
+
+        self.image = pygame.image.load('image/ship.png')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        # center, centerx, centery, top, bottom, left, right
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+        self.moving_right = False
+
+    def blitme(self):
+        """在指定位置绘制飞船"""
+        self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        if self.moving_right:
+            self.rect.centerx += 1
+            self.blitme()
